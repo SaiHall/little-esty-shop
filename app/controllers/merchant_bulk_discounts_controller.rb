59d07b20bf31122950acd698eb683947874f62e9
Sarah Hall <swatson1994@yahoo.com>
2022-06-10 22:00:30 -0400

    Feat: Add new discount tester view and update controller with new action

--- app/controllers/merchant_bulk_discounts_controller.rb
+++ app/controllers/merchant_bulk_discounts_controller.rb
@@ -7,4 +7,8 @@ class MerchantBulkDiscountsController < ApplicationController
   def show
     @discount = BulkDiscount.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
 end
