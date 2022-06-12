d959d93c76522d467cde642aa654dea6a6178429
Sarah Hall <swatson1994@yahoo.com>
2022-06-12 16:44:22 -0400

    Test: Add test for discounted difference add relationships

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -4,17 +4,25 @@ RSpec.describe InvoiceItem, type: :model do
   before :each do
     @billman = Merchant.create!(name: "Billman")
     @bracelet = @billman.items.create!(name: "Bracelet", description: "shiny", unit_price: 1001)
+    @mood = @billman.items.create!(name: "Mood Ring", description: "Moody", unit_price: 2002)
     @customer_1 = Customer.create!(first_name: 'Joey', last_name: "Ondricka")
     @invoice_1 = @customer_1.invoices.create!(status: "cancelled")
     @invoice_items_1 = @bracelet.invoice_items.create!(quantity: 1, unit_price: 1001, status: "Pending", invoice_id: @invoice_1.id)
-
     @parker = Merchant.create!(name: "Parker's Perfection Pagoda")
+
+    @brenda = Customer.create!(first_name: "Brenda", last_name: "Bhoddavista")
+    @invoice1 = @brenda.invoices.create!(status: "In Progress")
+    @order1 = @bracelet.invoice_items.create!(quantity: 1, unit_price: 1001, status: "Pending", invoice_id: @invoice1.id)
+    @order2 = @mood.invoice_items.create!(quantity: 5, unit_price: 2002, status: "Packaged", invoice_id: @invoice1.id)
+    @ten = @billman.bulk_discounts.create!(percentage: 0.10, threshold: 5)
 
   end
 
   describe 'relationships' do
     it {should belong_to :item}
     it {should belong_to :invoice}
+    it {should have_many :transactions}
+    it {should have_many :bulk_discounts}
   end
 
   describe 'validations' do
@@ -37,6 +45,11 @@ RSpec.describe InvoiceItem, type: :model do
     it 'belongs to merchant returns false if an invoice item does not belong to the given merchant' do
 
       expect(@invoice_items_1.belongs_to_merchant(@parker)).to eq(false)
+    end
+
+    it 'can show the amount discounted from an invoice_item' do
+
+      expect(@invoice1.invoice_items.discounted_difference).to eq(10.01)
     end
   end
 end
