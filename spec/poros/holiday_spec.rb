deccdffcd3e80525ebb57f240c368913331a13d6
Sarah Hall <swatson1994@yahoo.com>
2022-06-13 18:33:01 -0400

    Test: Add very minor tests to facades, update poro test to include date

--- spec/poros/holiday_spec.rb
+++ spec/poros/holiday_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Holiday do
   it 'exists and has attributes' do
-    data = { name: "Bear Day" }
+    data = { name: "Bear Day", date: "03/29/2023" }
     holiday = Holiday.new(data)
 
     expect(holiday).to be_an_instance_of(Holiday)
-    expect(holiday.name).to eq(data[:name])    
+    expect(holiday.name).to eq(data[:name])
   end
 end
